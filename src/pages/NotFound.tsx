cdee2ac5a91aa5b4f2670682e12819172eb6333f
anthony <elkommosyoussef.a@northeastern.edu>
2023-10-01 01:24:41 -0400

    Created Home, About, and NotFound pages

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+// Desc: 404 page
+const NotFound = () => {
+    return (
+        <div className="container">
+        <h1>404</h1>
+        <p>Page not found</p>
+        </div>
+    );
+}
+
+export default NotFound;
