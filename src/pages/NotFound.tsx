b64f172a2f0ab07aed2b5a906aa44f961cf6e9d6
anthony <elkommosyoussef.a@northeastern.edu>
2023-10-01 01:36:18 -0400

    Completed the NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,20 @@
+import { FaHome } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 // Desc: 404 page
 const NotFound = () => {
     return (
-        <div className="container">
-        <h1>404</h1>
-        <p>Page not found</p>
+        <div className='hero'>
+            <div className='text-center hero-content'>
+                <div className='max-w-lg'>
+                    <h1 className='text-8xl font-bold mb-8'>Oops!</h1>
+                    <p className='text-5xl mb-8'>404 - Page Not Found!</p>
+                    <Link className='btn btn-primary btn-lg' to='/'>
+                        <FaHome className='mr-2' />
+                        Back To Home
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
